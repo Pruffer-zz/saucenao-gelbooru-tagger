32e01740c6614f08f2500d8e0c818ad2fe81a867
Pruffer <pruffer@avantheim.org>
2020-10-25 21:48:33 +0100

    Add JSON output & other types of tags

--- main.py
+++ main.py
@@ -1,42 +1,46 @@
 # Import libs
-
 from saucenao_api import SauceNao
 from bs4 import BeautifulSoup
 import requests
+import json
 
 # Get image from Gelbooru DB & search
-
-results = SauceNao(db=25).from_url('IMAGE URL HERE')
+results = SauceNao(db=25).from_url('INSERT URL HERE')
 
 # Assign the variable 'best' the first result's URL & print
-
 best = ''.join(results[0].urls)
 print('URL Found: ' + best)
 
 # Get page
-
 html = requests.get(best).text
 
-# Parse tags' parent <li> elements from page
-
+# Parse tags' parent elements from page
 soup = BeautifulSoup(html, "lxml")
-list = soup.find_all("li", attrs={"class": "tag-type-general"})
 
-# Define empty tag list
+# Make list of tag types & prepare empty tag dictionary
+types = ['general', 'character', 'artist', 'copyright', 'metadata']
+result = dict.fromkeys(types, [])
 
-tags = []
+# For every type of tag..
+for item in types: 
+	# Find all <li> elements of tag type
+	list = soup.find_all("li", attrs={"class": "tag-type-" + item})
 
-# Add tags to list, ignore '?' link
+	# Make empty tag list
+	tags = []
 
-# For each <li>
-for item in list:
-	# Find all <a> elements
-	for a in item.find_all("a"):
-		# If <a> element's content isn't '?'
-		if ''.join(a.contents) != '?':
-			# Add it to the list
-			tags.append(''.join(a.contents))
+	# For each tag parent element..
+	for tag in list:
+		# Find all <a> elements
+		for a in tag.find_all("a"):
+			# If <a> element's content isn't '?', then..
+			if ''.join(a.contents) != '?':
+				# Add it to the list
+				tags.append(''.join(a.contents))
 
-# Print
+	# Put list into dictionary
+	result[item] = tags
 
-print(tags)
+# JSON-ify and print
+json = json.dumps(result, indent=4)
+print(json)
